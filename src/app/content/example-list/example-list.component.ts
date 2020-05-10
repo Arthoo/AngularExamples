748d483edf15ba00c5bfc4e0ee6c7d8b83642728
Oscar Lundahl <lundahl.oscar@gmail.com>
2020-05-10 18:05:54 +0200

    Generic list layout fixes

--- src/app/content/example-list/example-list.component.ts
+++ src/app/content/example-list/example-list.component.ts
@@ -8,26 +8,30 @@ import { Component } from '@angular/core';
 export class GenericListComponent {
   code = {
     listTs: `export class ListComponent implements OnInit {
+
+  // List item data
   @Input() items: any[];
 
-  // This is the reference to the template that is a child of the list
+  // Reference to the template with component to instantiate
   @ContentChild('itemTemplate') itemTemplate: TemplateRef<any>;
 
   ...
 `,
-    listHtml: `<ng-container *ngFor="let item of itemsDisplayed">
+    listHtml: `...
+<ng-container *ngFor="let item of itemsDisplayed">
 
-<!-- The provided template will be instantiated here for each item -->
-<ng-container *ngTemplateOutlet="itemTemplate; context: { item: item }">
+  <!-- The provided template will be instantiated here for each item -->
+  <ng-container *ngTemplateOutlet="itemTemplate; context: { item: item }">
+  </ng-container>
+
 </ng-container>
-
-</ng-container>`,
+...`,
     cats: `<app-list [title]="'Cats'" [items]="cats" [doesMatchSearch]="doesMatchSearch">
 
-<!-- This template contains the component that will be instantiated for each list item -->
-<ng-template #itemTemplate let-cat="item">
-  <app-cat [cat]="cat"></app-cat>
-</ng-template>
+  <!-- This template contains the component that will be instantiated for each list item -->
+  <ng-template #itemTemplate let-cat="item">
+    <app-cat [cat]="cat"></app-cat>
+  </ng-template>
 
 </app-list>`,
   };
