ae5ebd5c569394158ad005d51222070e00845e3b
Oscar Lundahl <lundahl.oscar@gmail.com>
2020-05-20 14:21:03 +0200

    list refactor

--- src/app/common/app.common.module.ts
+++ src/app/common/app.common.module.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CodeSectionComponent } from './code-section/code-section.component';
 import { ExpandableIconComponent } from './expandable-icon/expandable-icon.component';
-import { CommonModule } from '@angular/common';
-import { TestDirective } from './test.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CodeSectionComponent, ExpandableIconComponent, TestDirective],
-  exports: [CodeSectionComponent, ExpandableIconComponent],
+  declarations: [
+    CodeSectionComponent,
+    ExpandableIconComponent,
+  ],
+  exports: [
+    CodeSectionComponent,
+    ExpandableIconComponent,
+  ],
 })
 export class AppCommonModule {}
--- src/app/content/example-list/example-list.module.ts
+++ src/app/content/example-list/example-list.module.ts
@@ -3,12 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from 'src/app/app.component';
 import { AppCommonModule } from 'src/app/common/app.common.module';
-import { ListModule } from 'src/app/common/list/list.module';
 import { CatComponent } from './cats/cat/cat.component';
 import { CatsComponent } from './cats/cats.component';
 import { DogComponent } from './dogs/dog/dog.component';
 import { DogsComponent } from './dogs/dogs.component';
 import { ExampleListComponent } from './example-list.component';
+import { ListModule } from './list/list.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,13 @@ import { ExampleListComponent } from './example-list.component';
   ],
   exports: [ExampleListComponent],
   entryComponents: [ExampleListComponent],
-  imports: [AppCommonModule, ListModule, MatIconModule, MatButtonModule],
+  imports: [
+    AppCommonModule,
+    ListModule,
+    MatIconModule,
+    MatButtonModule,
+    AppCommonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
